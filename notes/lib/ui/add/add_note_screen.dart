7bebf98bdfd0fa0bea00b688c2959f236ebc53d4
Julio Lemus <julio.lemus.lara@gmail.com>
2019-06-19 20:44:20 -0500

    First Screen: -AddNoteScreen added to the project

---
+++ notes/lib/ui/add/add_note_screen.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/widgets.dart';
+
+class AddNoteScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _AddNoteState();
+  }
+}
+
+class _AddNoteState extends State<AddNoteScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("Hello world"));
+  }
+
+}
